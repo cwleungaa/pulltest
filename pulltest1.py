19ad2c8ac4fd4716c62d1a412b46c8cc6669073a
cwleungaa <47685520+cwleungaa@users.noreply.github.com>
2021-09-12 17:05:33 +0800

    Update pulltest1.py

--- pulltest1.py
+++ pulltest1.py
@@ -3,3 +3,5 @@ print("The first original sentence in git.")
 print("This sentence is using for creating new branch.")
 
 print("Second commit for cat branch")
+
+print("This line is for the pull update,if appear locally = success")
